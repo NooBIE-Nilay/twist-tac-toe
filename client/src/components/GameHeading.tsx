40ac2ad0488011b42ef16ecdf6cd4bf1decdfe30
Nilay Banerjee <n00bie@Nilays-MacBook-Air.local>
2024-09-09 23:41:23 +0530

    Somewhat Fixed Landing Page

--- client/src/components/GameHeading.tsx
+++ client/src/components/GameHeading.tsx
@@ -3,8 +3,8 @@ import WordRotate from "@/components/magicui/word-rotate"
 export function GameHeading() {
     return (
         <>
-            <div className="grid grid-flow-col items-center justify-center text-8xl font-bold">
-                <div className="min-w-[239.27px]">
+            <div className="grid grid-flow-col items-center justify-center text-6xl font-bold md:text-8xl">
+                <div className="min-w-[148px] md:min-w-[244px]">
                     <WordRotate
                         className="text-end text-black dark:text-white"
                         words={["Tic", "Twist"]}
