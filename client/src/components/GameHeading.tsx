95c7ea1322ceaabd506e7b74688d5806bceb20dc
Nilay Banerjee <n00bie@Nilays-MacBook-Air.local>
2024-09-09 20:22:05 +0530

    Added Responsiveness in Landing Page

--- client/src/components/GameHeading.tsx
+++ client/src/components/GameHeading.tsx
@@ -3,8 +3,8 @@ import WordRotate from "@/components/magicui/word-rotate";
 export function GameHeading() {
   return (
     <>
-      <div className="grid grid-flow-col justify-center items-center text-8xl font-bold ">
-        <div className="min-w-[239.27px]">
+      <div className=" grid grid-flow-col justify-center items-center text-6xl md:text-8xl font-bold ">
+        <div className="min-w-[150px] md:min-w-[240px]">
           <WordRotate
             className="  text-black dark:text-white text-end "
             words={["Tic", "Twist"]}
