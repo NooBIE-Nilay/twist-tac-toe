4ceb066d4b52a8a1890b7586a0753cbf6cbd5485
Nilay Banerjee <n00bie@Nilays-MacBook-Air.local>
2024-09-10 23:20:58 +0530

    Added Responsive Design to Join Page

--- client/src/components/pages/Join.tsx
+++ client/src/components/pages/Join.tsx
@@ -18,6 +18,8 @@ export function Join({
 }) {
     const [gameId, setGameId] = useState(useParams().joinId || "")
     const [isLoading, setIsLoading] = useState(false)
+    const [isError, setIsError] = useState(false)
+    const [errorMsg, setErrorMsg] = useState("")
     const navigate = useNavigate()
     const { theme } = useTheme()
 
@@ -32,9 +34,26 @@ export function Join({
     }, [events])
     function joinGame() {
         if (!socket.connected) socket.connect()
-        if (!gameId) return
+        if (!gameId) {
+            setIsError(true)
+            setErrorMsg("Please Enter A Game Code!")
+            setTimeout(() => {
+                setIsError(false)
+                setErrorMsg("")
+            }, 2500)
+            return
+        }
         setIsLoading(true)
+        setIsError(false)
         socket.emit("joinGame", { username, gameId })
+        setTimeout(() => {
+            setIsLoading(false)
+            setIsError(true)
+            setErrorMsg("GameID not found")
+            setTimeout(() => {
+                setIsError(false)
+            }, 2000)
+        }, 3000)
         return () => {
             socket.disconnect()
         }
@@ -45,25 +64,37 @@ export function Join({
             <div className="flex justify-center">
                 <div>
                     <MagicCard
-                        className="top-[50%] h-[280px] w-[460px] cursor-pointer flex-col items-center justify-center whitespace-nowrap text-2xl shadow-2xl"
+                        className="top-[50%] cursor-pointer flex-col items-center justify-center whitespace-nowrap p-6 text-2xl shadow-2xl md:h-[280px]"
                         gradientColor={
                             theme === "dark" ? "#262626" : "#D9D9D955"
                         }
                     >
-                        <div className="flex flex-col justify-center gap-6 text-wrap">
-                            <span className="text-4xl capitalize">
+                        <div className="flex flex-col justify-center gap-2 text-wrap md:gap-6">
+                            <span className="text-lg capitalize md:text-4xl">
                                 Enter Game Code:
                             </span>
-                            <div className="inline-flex gap-5">
-                                <InputCodePattern
-                                    setCode={setGameId}
-                                    value={gameId}
-                                />
-                                <Button variant="outline" onClick={joinGame}>
-                                    Join Game
-                                </Button>
+                            <div className="flex flex-col items-center justify-center gap-4 md:inline-flex md:flex-row">
+                                <div className="">
+                                    <InputCodePattern
+                                        setCode={setGameId}
+                                        value={gameId}
+                                    />
+                                </div>
+                                <div>
+                                    <Button
+                                        variant="outline"
+                                        onClick={joinGame}
+                                    >
+                                        Join Game
+                                    </Button>
+                                </div>
                             </div>
                             {isLoading && <Loading />}
+                            {isError && (
+                                <div className="flex items-center justify-start text-xl capitalize text-red-500/90">
+                                    {errorMsg}
+                                </div>
+                            )}
                         </div>
                     </MagicCard>
                 </div>
